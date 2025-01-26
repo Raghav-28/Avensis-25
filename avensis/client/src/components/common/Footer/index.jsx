e1446398c5699a9e29c14075b2f99ea454ee8328
Raghav-28 <kraghavgupta@gmail.com>
2025-01-26 12:10:05 +0530

    Updated dependencies

--- avensis/client/src/components/common/Footer/index.jsx
+++ avensis/client/src/components/common/Footer/index.jsx
@@ -1,70 +1,9 @@
-import React, { useState } from "react";
-import image from "../../../Images/footer-back.png";
+import React from "react";
+// import image from "../../../Images/footer-back.png";
 import { FiMail } from "react-icons/fi";
 import { FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
-  const [count, setCount] = useState(0);
-  function displayTeam() {
-    setCount(1);
-  }
-  function hideTeam() {
-    setCount(0);
-  }
-  /* const team = [
-    {
-      name: "Tanay Aggarwal",
-      linkedIn:"https://www.tanayaggarwal.com",
-      image : tanay,
-    },
-    {
-      name: "Utkarsh Ahuja",
-      linkedIn:"https://linktr.ee/ahuja_utkarsh",
-      image : utkarsh,
-    },
-    {
-      name: "Uday Sangwan",
-      linkedIn:"https://linktr.ee/uday_sangwan",
-      image : uday,
-    },
-    {
-      name: "Mohd Kaif",
-      linkedIn:"https://www.linkedin.com/in/mohd-kaif-7ba250228",
-      image : kaif,
-    },
-    {
-      name: "Yash Chaudhary",
-      linkedIn:"https://www.linkedin.com/in/yash-c88/",
-      image : yash,
-    },
-    {
-      name: "Anshita Chaudhary",
-      linkedIn:"https://www.linkedin.com/in/anshita-chaudhary-7b0272253",
-      image : anshita,
-    },
-    {
-      name: "Ankit Chaudhary",
-      linkedIn:"https://www.linkedin.com/in/ankit-chaudhary-01a534228/",
-      image : chutia,
-    },
-    {
-      name: "Kratika Suri",
-      linkedIn:"https://www.linkedin.com/in/kratika-suri-429229228",
-      image : kratika,
-    },{
-      name: "Harsh Jain",
-      linkedIn:"https://www.linkedin.com/in/harsh-jain-10467a22b",
-      image : harsh,
-    },
-  ]; */ 
-  // function memberCard(member)
-  // {
-  //   return <div class="mx-[15px] w-[160px]">
-  //               <a href={member.linkedIn} target="_blank"><img src={member.image} className=' mx-auto rounded-[50%] w-36 h-36'/></a>
-  //               <h4 className='text-[18px] text-black font-semibold'>{member.name}</h4>
-  //               <a href={member.linkedIn}><h5 className='font-bold text-[16px] text-blue-600 '>Linkedin</h5></a>
-  //           </div>
-  // }
   return (
     <div id="footer"  className='m-0 bg-[url(https://i.ibb.co/vhQq6hM/footer-back.png)] '>
       <div className='pb-8'>
@@ -186,23 +125,7 @@ Tarush Masand
           </div>
         </div>
 
-        {count === 1 && (
-          <div className=" w-[80%] mx-auto bg-white rounded-lg text-black fixed top-[5%] left-[10%]">
-            <h1 className="py-3 ml-[10%] text-3xl font-semibold text-black">
-              Meet the team
-            </h1>
-            <div className="w-full h-[2px] bg-gray-300"></div>
-            <div className="w-[90%] flex flex-wrap mx-auto justify-around text-center mt-3">
-              {/*
-                        {team.map((member) => (
-                          memberCard(member)
-                        ))} 
-                     */ }
-                </div>
-                <div className='w-full h-[2px] bg-gray-300'></div>
-                <button onClick={hideTeam} type="button" id="closeteam" className='ml-[85%] my-2' data-dismiss="modal">Close</button>
-              </div>
-                 }
+        
              </div>
       
     </div>
